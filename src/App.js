8527850faa20e5178d662bc5dff8d25b90278f72
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-10-11 14:28:39 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import fetchPosts from './FetchApi';
+import { useQuery } from 'react-query'
+import Form from './Form';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React Query</h1>
-    </div>
-  );
+    const { data, error, isError, isLoading } = useQuery('users', fetchPosts)
+    
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (isError) {
+        return <div>Error! {error.message}</div>
+    }
+    
+    return (
+        <div className=''>
+            <h1 className='container'>Users Name</h1>
+            {
+                data.map((users, id) => {
+                    return <li className='container' key={id}>{users.name}</li>
+                })
+            }
+            <Form />
+        </div>
+    )
 }
-
+    
 export default App;
